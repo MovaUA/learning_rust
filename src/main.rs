54fb2282ef6b07937fe07a01d73e583262bc9c03
Valeriy <valeriy.molchanov.77@gmail.com>
2024-01-10 16:47:31 +0100

    Implement exercise: meadian and mode (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use std::collections::HashMap;
-
+mod median;
 mod pig_latin;
 
 fn main() {
+    // Given a list of integers,
+    // use a vector and return
+    // the median (when sorted, the value in the middle position)
+    // and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+    let empty: Vec<i32> = vec![];
+
+    do_the_job(&empty);
+    do_the_job(&vec![1]);
+    do_the_job(&vec![5, 3, 4]);
+    do_the_job(&vec![1, 2, 3, 2]);
+    do_the_job(&vec![1, 2, 1]);
+
+
     // Convert strings to pig latin.
     // The first consonant of each word is moved to the end of the word and “ay” is added,
     // so “first” becomes “irst-fay.”
@@ -16,3 +28,14 @@ fn main() {
         original_text, pig_latin_text
     );
 }
+
+
+fn do_the_job(v: &[i32]) {
+    println!("{:?}", v);
+
+    println!("median: {:?}", median::median(&v));
+    println!("mode: {:?}", median::mode(&v));
+
+    println!()
+}
+
